6e43a5c3da585820606ee724f8a1eced7a4326f9
audreyyun <96audreyyun@gmail.com>
2020-12-29 15:18:24 -0500

    Finish drawing and moving

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 const MovingObject = require('./moving_object');
-
-
+window.MovingObject = MovingObject; //just so we have access so we can test
 document.addEventListener('DOMContentLoaded', function(){
-
+    
     const canvasEl = document.getElementById('game-canvas');
-
-
-const ctx = canvasEl.getContext('2d');
-MovingObject.prototype.draw(ctx);
-
-
+    const ctx = canvasEl.getContext('2d');
+    window.ctx = ctx; //just so we have access so we can test
 
 });
--- src/moving_object.js
+++ src/moving_object.js
@@ -5,7 +5,17 @@ function MovingObject (options) {
     this.color = options.color;
 }
 
+MovingObject.prototype.draw = function (ctx) {
+    ctx.beginPath();
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI);
+    ctx.stroke();
 
+}
 
+MovingObject.prototype.move = function () { 
+    this.pos[0] += this.vel[0]
+    this.pos[1] += this.vel[1]
+}
+ 
 module.exports = MovingObject;
 
